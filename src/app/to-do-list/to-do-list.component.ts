9f6065205d6ae93b645ab7a22e7e841c180ee395
Daan Keun <daan.keun@entelect.co.za>
2020-10-09 12:02:44 +0200

    Add input to specify item

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -8,7 +8,8 @@ import { Component, Input } from '@angular/core';
 export class ToDoListComponent {
     @Input() thingsToDo: string[];
 
-    addToDo(toDo: string) {
-        this.thingsToDo.push(toDo);
+    addToDo(toDo: HTMLInputElement) {
+        this.thingsToDo.push(toDo.value);
+        toDo.value = '';
     }
 }
