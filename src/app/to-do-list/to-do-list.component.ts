ef71de2e75ba67044c0dce0ff35e507a054cd912
Daan Keun <daan.keun@entelect.co.za>
2020-10-12 11:53:45 +0200

    Refactor to use two-way binding

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-to-do-list',
-  templateUrl: './to-do-list.component.html',
-  styleUrls: ['./to-do-list.component.scss']
+    selector: 'app-to-do-list',
+    templateUrl: './to-do-list.component.html',
+    styleUrls: ['./to-do-list.component.scss']
 })
 export class ToDoListComponent {
     @Input() thingsToDo: string[];
+    @Output() thingsToDoChange = new EventEmitter<string[]>();
+
+    onComplete(index): void {
+        const thingsToDo = this.thingsToDo.filter((item, idx) => idx !== index);
+        this.thingsToDoChange.emit(thingsToDo);
+    }
 }
