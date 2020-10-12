0138d0201e30c43496e4728426de3f7ceb35dc7a
Daan Keun <daan.keun@entelect.co.za>
2020-10-12 13:57:37 +0200

    Add ContentChild to add item to list

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild } from '@angular/core';
+import { ToDoListComponent } from '../to-do-list/to-do-list.component';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements AfterContentInit {
     cardColour = '#FFF';
+
+    @ContentChild(ToDoListComponent, {static: false}) toDoListComponent: ToDoListComponent;
+
+    ngAfterContentInit() {
+        const thingsToDo = this.toDoListComponent.thingsToDo;
+        thingsToDo.push('Testing');
+
+        this.toDoListComponent.thingsToDoChange.emit(thingsToDo);
+    }
+
 }
